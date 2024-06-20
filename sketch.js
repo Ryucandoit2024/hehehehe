fb9808fb584b97bdec9d329639e0cbd3f95538e2
Ryucandoit2024 <168039220+Ryucandoit2024@users.noreply.github.com>
2024-06-20 09:18:05 +0900

    hatter

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 var pictureList; //this variable is used in preload() and in draw() so we create it outside of those sections.
+var pictureList2;
 let catImg;
 
 function preload() {
   pictureList = [];
   
-  var imgCount = 7; //total number of images
+  var imgCount = 14; //total number of images
   //use a for loop to load all 7 images into the array
   for (var i = 0; i < imgCount; i++) {
     pictureList[i] = loadImage((i + 1) + ".png");
+    
     catImg = loadImage('cat.png');
+  }
+  
+  pictureList2 = [];
+  
+  var imgCount2 = 14; //total number of images
+  //use a for loop to load all images into the array
+  for (var i = 7; i < imgCount2; i++) {
+    pictureList2[i] = loadImage((i + 1) + ".png");
   }
 }
 
@@ -58,8 +68,8 @@ function draw() {
     var r = int(random(0, 7)); 
     var randomImage = pictureList[r] 
     
-    var x = int(random(-100, 300));
-    var y = int(random(-100, 300));
+    var x = int(random(-100, 400));
+    var y = int(random(-100, 400));
      
     image(randomImage, x, y, randomImage.width, randomImage.height);
     rotate(HALF_PI / 11.25)
@@ -68,7 +78,7 @@ function draw() {
   
   
   //save("final-1.jpg"); //uncomment to save images; never use without the noLoop() in setup()
-
+  //고양이사진
   // let r = random(255);
   // let g = random(255);
   // let b = random(255);
@@ -76,13 +86,28 @@ function draw() {
   
      {
     
-    let r = random(255);
-    let g = random(255);
-    let b = random(255);
+    let r = random(200, 255);
+    let g = random(150 ,255);
+    let b = random(0, 255);
     tint(r, g, b);
     image(catImg, 0, 0, width, height);
   }
   
+
+  var n = 1; //악세서리images
+
+  for (var i = 0; i < n; i++) {
+    
+    var r = int(random(0, 14)); 
+    var randomImage2 = pictureList2[r] 
+    
+    var x = 150;
+    var y = -10;
+     
+    image(randomImage2, x, y, 200, 200);
+    
+  }
+
 }
 
 function mouseClicked() {
